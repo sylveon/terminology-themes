b2ce94b8a79d782372cec79c7f983b16699ee288
Charles Milette <charles.milette@gmail.com>
2017-10-23 17:12:04 -0400

    Take file to parse as command line argument

--- tools/convert.py
+++ tools/convert.py
@@ -21,51 +21,55 @@ def print_lines(lines_to_print):
 
 
 def main():
-    import configparser
-    config = configparser.ConfigParser()
+    import errno, sys
+    if len(sys.argv) == 2:
+        import configparser
+        config = configparser.ConfigParser()
 
-    config.read('AdventureTime.theme')
-    scheme = config['Scheme']
+        config.read(sys.argv[1])
+        scheme = config['Scheme']
 
-    print_define('COL_DEF',
-                 rgb_tuple_to_string(decode_hex(scheme['colorforeground'])) + ' 255',
-                 'Foreground color')
-    print()
+        print_define('COL_DEF',
+                     rgb_tuple_to_string(decode_hex(scheme['colorforeground'])) + ' 255',
+                     'Foreground color')
+        print()
 
-    for (index, ansi_color) in enumerate(scheme['colorpalette'].split(';')):
-        if index == 8:
-            print()
-        elif index == 1:
-            ansi1_string = rgb_tuple_to_string(decode_hex(ansi_color))
+        for (index, ansi_color) in enumerate(scheme['colorpalette'].split(';')):
+            if index == 8:
+                print()
+            elif index == 1:
+                ansi1_string = rgb_tuple_to_string(decode_hex(ansi_color))
 
-        print_define('ANSI' + str(index),
-                     rgb_tuple_to_string(decode_hex(ansi_color)) + ' 255')
+            print_define('ANSI' + str(index),
+                         rgb_tuple_to_string(decode_hex(ansi_color)) + ' 255')
 
-    print()
-    print_define('BG_COLOR',
-                 rgb_tuple_to_string(decode_hex(scheme['colorbackground'])) + ' 255')
-    print_lines(4)
+        print()
+        print_define('BG_COLOR',
+                     rgb_tuple_to_string(decode_hex(scheme['colorbackground'])) + ' 255')
+        print_lines(4)
 
-    print_define('BELL', 'ANSI1', 'Color of the bell')
-    print()
+        print_define('BELL', 'ANSI1', 'Color of the bell')
+        print()
 
-    print_define('BELL_OVERLAY_COLOR', ansi1_string + ' 100',
-                 'Color flash when bell rings')
-    print()
+        print_define('BELL_OVERLAY_COLOR', ansi1_string + ' 100',
+                     'Color flash when bell rings')
+        print()
 
-    print_define('CURSOR',
-                 rgb_tuple_to_string(decode_hex(scheme['colorcursor'])),
-                 'Cursor color')
-    print()
+        print_define('CURSOR',
+                     rgb_tuple_to_string(decode_hex(scheme['colorcursor'])),
+                     'Cursor color')
+        print()
 
-    print_define('LINK', 'ANSI4', 'Color of the link underline')
-    print()
+        print_define('LINK', 'ANSI4', 'Color of the link underline')
+        print()
 
-    print_define('MEDIA_BG', 'BG_COLOR',
-                 'Background color of the popmedia')
-    print_lines(4)
+        print_define('MEDIA_BG', 'BG_COLOR',
+                     'Background color of the popmedia')
+        print_lines(4)
 
-    print('#include "../base.edc"')
+        print('#include "../base.edc"')
+    else:
+        raise ValueError("Invalid number of parameters passed")
 
 
 if __name__ == '__main__': main()
