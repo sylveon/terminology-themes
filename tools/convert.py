78e2b0ceddfceca5a92aa3f8dd06f7e1c678a86e
Charles Milette <charles.milette@gmail.com>
2017-10-23 17:03:03 -0400

    woops

--- tools/convert.py
+++ tools/convert.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 def print_define(define_name, define_value, define_comment=''):
     if define_comment:
         define_comment = ' // ' + define_comment
-    print '#define ' + define_name + ' ' + define_value + define_comment
+    print('#define ' + define_name + ' ' + define_value + define_comment)
 
 
 def decode_hex(hex_code):
-    return tuple(int(hex_code.lstrip('#')[i:i + 2], 16) for i in (0, 2,
-                 4))
+    return tuple(int(hex_code.lstrip('#')[i:i + 2], 16) for i in (0, 2, 4))
 
 
 def rgb_tuple_to_string(rgb_tuple):
@@ -18,7 +17,7 @@ def rgb_tuple_to_string(rgb_tuple):
 
 def print_lines(lines_to_print):
     for _ in range(lines_to_print):
-        print ()
+        print()
 
 
 def main():
@@ -29,45 +28,44 @@ def main():
     scheme = config['Scheme']
 
     print_define('COL_DEF',
-                 rgb_tuple_to_string(decode_hex(scheme['colorforeground'
-                 ])) + ' 255', 'Foreground color')
-    print ()
+                 rgb_tuple_to_string(decode_hex(scheme['colorforeground'])) + ' 255',
+                 'Foreground color')
+    print()
 
     for (index, ansi_color) in enumerate(scheme['colorpalette'].split(';')):
         if index == 8:
-            print ()
+            print()
         elif index == 1:
             ansi1_string = rgb_tuple_to_string(decode_hex(ansi_color))
 
         print_define('ANSI' + str(index),
                      rgb_tuple_to_string(decode_hex(ansi_color)) + ' 255')
 
-    print ()
+    print()
     print_define('BG_COLOR',
                  rgb_tuple_to_string(decode_hex(scheme['colorbackground'])) + ' 255')
     print_lines(4)
 
     print_define('BELL', 'ANSI1', 'Color of the bell')
-    print ()
+    print()
 
     print_define('BELL_OVERLAY_COLOR', ansi1_string + ' 100',
                  'Color flash when bell rings')
-    print ()
+    print()
 
     print_define('CURSOR',
                  rgb_tuple_to_string(decode_hex(scheme['colorcursor'])),
                  'Cursor color')
-    print ()
+    print()
 
     print_define('LINK', 'ANSI4', 'Color of the link underline')
-    print ()
+    print()
 
     print_define('MEDIA_BG', 'BG_COLOR',
                  'Background color of the popmedia')
     print_lines(4)
 
-    print '#include "../base.edc"'
+    print('#include "../base.edc"')
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': main()
