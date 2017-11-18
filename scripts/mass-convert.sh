d2f5afab8d815907fe7014eb86f08ddba11eba60
Charles Milette <charles.milette@gmail.com>
2017-11-18 14:42:31 -0500

    Add help to mass convert

--- scripts/mass-convert.sh
+++ scripts/mass-convert.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-[ -z "${2}" ] && echo "Invalid number of parameters passed" && exit 1
+[[ ${1} == "--help" ]] && echo "usage: mass-convert.sh [source directory] [output directory]" && \
+                          echo "You can optionally append --remove-whitespaces to remove whitespaces in the name of the output files" && exit
+
+[ -z "${2}" ] && echo "Invalid number of parameters passed" && echo "See '--help' for usage" && exit 1
 
 SCRIPT=$(readlink -f ${0})
 SCRIPT_FOLDER=`dirname ${SCRIPT}`
