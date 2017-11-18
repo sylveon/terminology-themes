fe6fd7e960f679af2c024540c1549ba6d1d45a0a
Charles Milette <charles.milette@gmail.com>
2017-11-18 14:54:11 -0500

    Shellcheck fixes

--- scripts/mass-convert.sh
+++ scripts/mass-convert.sh
@@ -5,18 +5,18 @@
 
 [ -z "${2}" ] && echo "Invalid number of parameters passed" && echo "See '--help' for usage" && exit 1
 
-SCRIPT=$(readlink -f ${0})
-SCRIPT_FOLDER=`dirname ${SCRIPT}`
+SCRIPT=$(readlink -f "${0}")
+SCRIPT_FOLDER=$(dirname "${SCRIPT}")
 
-cd ${1}
+cd "${1}" || exit 2
 
 IFS=$'\n'
-for file in `find -type f`
+for file in $(find -type f)
 do
-	cd ${SCRIPT_FOLDER}
-	OUTPUT_FILE=${2}/`basename ${file} .theme`.edc
-	./convert.py ${1}/${file} > ${OUTPUT_FILE}
-	[[ ${3} == "--remove-whitespaces" ]] && rename -f "s/\s+//g" ${OUTPUT_FILE}
+	cd "${SCRIPT_FOLDER}" || exit 2
+	OUTPUT_FILE=${2}/$(basename "${file}" .theme).edc
+	./convert.py "${1}/${file}" > "${OUTPUT_FILE}"
+	[[ ${3} == "--remove-whitespaces" ]] && rename -f "s/\s+//g" "${OUTPUT_FILE}"
 done
 
 echo "Done!"
